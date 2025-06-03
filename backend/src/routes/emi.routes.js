15016a59fc27d857212bfacb063524a2d1d62c56
BeyonderSS <ssbeyonder@gmail.com>
2025-06-03 17:06:11 +0530

    update the integration of emi page for users

--- backend/src/routes/emi.routes.js
+++ backend/src/routes/emi.routes.js
@@ -1,9 +1,11 @@
-import { getUserEmiHistory, getUserEmiHistoryByUser } from "../controllers/emi.controller.js";
+import {
+  getUserEmiHistory,
+  getUserEmiHistoryByUser,
+} from "../controllers/emi.controller.js";
 import express from "express";
 const router = express.Router();
 
-
-router.get("history/user/:userId",getUserEmiHistory);
-router.get("details/user/:userId",getUserEmiHistoryByUser);
+router.get("/history/user/:userId", getUserEmiHistory);
+router.get("/details/user/:userId", getUserEmiHistoryByUser);
 
 export default router;
