be9afadbccf67502f84764b4e31400626496a28b
Priyanshu sahu <priyanshusahupanagar@gmail.com>
2025-06-03 16:46:47 +0530

    emi

--- frontend/constant/config.js
+++ frontend/constant/config.js
@@ -1,5 +1,7 @@
 export const backendConfig = {
-  base: " http://localhost:5000/api",
+  // base: " http://localhost:5000/api",
+  // origin: "https://makemoney24.onrender.com",
+  base: " https://4sr8mplp-3000.inc1.devtunnels.ms/api",
   origin: "https://makemoney24.onrender.com",
 };
 
