7e631d6f7fe80ccce4d082d5d7f771dd175a4bb2
BeyonderSS <ssbeyonder@gmail.com>
2025-06-11 15:52:54 +0530

    updated config

--- frontend/constant/config.js
+++ frontend/constant/config.js
@@ -5,13 +5,13 @@ export const backendConfig = {
 
   // base: " https://api.makemoney24hrs.com/api",
   // origin: "https://api.makemoney24hrs.com",
-  
-  // // base: " https://api.makemoney24hrs.com/api",
-  // // origin: "https://api.makemoney24hrs.com/
+
+  base: " https://api.makemoney24hrs.com/api",
+  origin: "https://api.makemoney24hrs.com/"
 
 
-  base: " http://localhost:3000/api",
-  origin: "http://localhost:3000/",
+  // base: " http://localhost:3000/api",
+  // origin: "http://localhost:3000/",
 
 
   // base: "https://4sr8mplp-3000.inc1.devtunnels.ms/api",
