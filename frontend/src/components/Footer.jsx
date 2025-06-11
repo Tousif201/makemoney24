7cd26b58d50964255b29a99c1d5eadf89dc19bb4
Priyanshu sahu <priyanshusahupanagar@gmail.com>
2025-06-11 15:05:16 +0530

    fix issue and fronted

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -16,8 +16,8 @@ const MobileBottomNav = () => {
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 md:hidden z-50">
       <div className="flex justify-around items-center py-2">
-        <Link to="/" className="p-2 text-purple-600">
-          <div className="bg-purple-100 p-2 rounded-lg">
+        <Link to="/" className="p-2 text-gray-600">
+          <div className=" p-2 rounded-lg">
             <Home size={22} />
           </div>
           Home
