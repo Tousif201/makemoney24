3ff0becc0b52ade6b47a46a7027c83fd265df340
maneesh1105 <maneeshsharma1105@gmail.com>
2025-06-11 16:45:15 +0530

    dadf

--- frontend/constant/config.js
+++ frontend/constant/config.js
@@ -3,12 +3,7 @@ export const backendConfig = {
   // // base: " https://api.makemoney24hrs.com/api",
   // // origin: "https://api.makemoney24hrs.com",
 
-  // base: " https://api.makemoney24hrs.com/api",
-  // origin: "https://api.makemoney24hrs.com",
-  
-  // // base: " https://api.makemoney24hrs.com/api",
-  // // origin: "https://api.makemoney24hrs.com/
-
+ 
 
   base: " http://localhost:3000/api",
   origin: "http://localhost:3000/",
